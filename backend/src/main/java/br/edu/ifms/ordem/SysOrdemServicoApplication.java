b88d6d89509b96bb3b6c08e95a20d342487fdb2f
Marcos Rogerio Ferreira <2413666@ifms.edu.br>
2022-08-19 21:01:33 -0400

    CORS e Swagger

--- backend/src/main/java/br/edu/ifms/ordem/SysOrdemServicoApplication.java
+++ backend/src/main/java/br/edu/ifms/ordem/SysOrdemServicoApplication.java
@@ -2,10 +2,22 @@ package br.edu.ifms.ordem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class SysOrdemServicoApplication {
+@EnableWebMvc
+public class SysOrdemServicoApplication implements WebMvcConfigurer {
 
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		// TODO Auto-generated method stub
+		//WebMvcConfigurer.super.addCorsMappings(registry);
+		registry.addMapping("/**")
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SysOrdemServicoApplication.class, args);
 	}
